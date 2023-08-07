369fd44b0bdcd15f5e36ab1b4753cf2f2d2ec2ba
Alexon <mr.alexdemonp2003@mail.ru>
2023-08-07 13:34:40 +0300

    lesson_2_first symbol

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,11 @@
 function ucFirst(str) {
-  // ваш код...
+  if (str===null){
+    return(str.charAt(0).toUpperCase() + str.slice(1));
+  }
+  if (str.length=1){
+    return(str.charAt(0).toUpperCase() );
+  }
+  else{
+    return(str.charAt(0).toUpperCase() + str.slice(1));
+  }
 }
