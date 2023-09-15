7fefba2419203cca48c68c7fed046f7300d7a374
Alexon <mr.alexdemonp2003@mail.ru>
2023-09-15 17:40:41 +0300

    dz-4

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,9 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+  const rows = table.rows;
+  for (let i = 0; i < rows.length; i++) {
+    const row = rows[i];
+    if (row.cells[i]) {
+      row.cells[i].style.backgroundColor = 'red';
+    }
+  }
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,36 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.querySelectorAll('tbody tr');
+  rows.forEach(row => {
+    const cells = row.cells;
+
+    const statusCell = cells[3];
+    if (statusCell) {
+      const status = statusCell.getAttribute('data-available');
+      if (status === 'true') {
+        row.classList.add('available');
+      } else if (status === 'false') {
+        row.classList.add('unavailable');
+      } else {
+        row.hidden = true;
+      }
+    }
+
+    const genderCell = cells[2];
+    if (genderCell) {
+      const gender = genderCell.textContent;
+      if (gender === 'm') {
+        row.classList.add('male');
+      } else if (gender === 'f') {
+        row.classList.add('female');
+      }
+    }
+
+    const ageCell = cells[1];
+    if (ageCell) {
+      const age = parseInt(ageCell.textContent);
+      if (age < 18) {
+        row.style.textDecoration = 'line-through';
+      }
+    }
+  });
 }
